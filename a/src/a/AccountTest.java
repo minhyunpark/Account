af91d86b8f9e07d4dd0240dfc11003360fcf15cc
KAU <KAU@KAU-PC>
2017-03-20 16:43:59 +0900

    수정

--- a/src/a/AccountTest.java
+++ a/src/a/AccountTest.java
@@ -15,12 +15,12 @@ public class AccountTest {
 		System.out.printf("account2 balance : $%.2f\n",ac2.getBalance());
 		System.out.printf("\nEnter withdrawal amount for account%d :");
 		Scanner sc = new Scanner(System.in);
-		double sub = sc.nextDouble();
-		ac1.debit(sub);
-		System.out.printf("\nsubtracting %.2f from account%d balance \n",sub);
+		double m = sc.nextDouble();
+		ac1.debit(m);
+		System.out.printf("\nsubtracting %.2f from account%d balance \n",m);
 		System.out.printf("account1 balance : $%.2f\n",ac1.getBalance());
 		System.out.printf("account2 balance : $%.2f\n",ac2.getBalance());
-		ac2.debit(sub);
+		ac2.debit(m);
 		System.out.printf("account1 balance : $%.2f\n",ac1.getBalance());
 		System.out.printf("account2 balance : $%.2f\n",ac2.getBalance());
 		
